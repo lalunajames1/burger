243404b66c34aa834cba7f7af58fc625df6aed27
Luis Luna <luisluna@GHC-SEO-0032.globalhc.io>
2018-08-07 20:27:33 -0500

    my project

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('The Monster');
+INSERT INTO burgers (burger_name) VALUES ('Meatless Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Can Not Finish It', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Original', true);
